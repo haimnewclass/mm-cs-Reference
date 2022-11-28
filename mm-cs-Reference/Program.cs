805c08f24f28f58310e8576fcd0ad2057423d57a
haim hod gilboa <haim@newclass.co.il>
2022-11-28 10:30:15 +0200

    Try

--- mm-cs-Reference/Program.cs
+++ mm-cs-Reference/Program.cs
@@ -16,23 +16,58 @@ namespace mm_cs_Reference
         }
         static void Main(string[] args)
         {
-            int a;
-            GetA();
-            mm_cs_Reference.Patterns.Week9_2_Singletone week9_2_Singletone = new mm_cs_Reference.Patterns.Week9_2_Singletone();
-            week9_2_Singletone.Run();
 
-            Week9_1_Consts.Run();
-            Week8_7_ClassParty week8_7_ClassParty = new Week8_7_ClassParty();
-            week8_7_ClassParty.Run();
+            try
+            {
+                int r = Week10_2_Exception.Instance.test(10, 0);
+                if(r==-999999)
+                {
+                    r = Week10_2_Exception.Instance.test(10, 0);
+                }
+                try
+                {
+                    int a;
+                    GetA();
+                    mm_cs_Reference.Patterns.Week9_2_Singletone week9_2_Singletone = new mm_cs_Reference.Patterns.Week9_2_Singletone();
+                    week9_2_Singletone.Run();
+                }
+                catch(Exception e)
+                {
 
+                }
 
+                try
+                {
+                    Week9_1_Consts.Run();
+                    Week8_7_ClassParty week8_7_ClassParty = new Week8_7_ClassParty();
+                    week8_7_ClassParty.Run();
+                }
+                catch(Exception e)
+                {
 
-            Week8_5_ClassFactory.Run();
+                }
 
-            // Week8_5_Statis.Student student = new Week8_5_Statis.Student("234234");
-            Week8_5_Statis week8_5_Statis = new Week8_5_Statis();
-            week8_5_Statis.Ran();
+                try
+                {
+                    Week8_5_ClassFactory.Run();
 
+                    // Week8_5_Statis.Student student = new Week8_5_Statis.Student("234234");
+                    Week8_5_Statis week8_5_Statis = new Week8_5_Statis();
+                    week8_5_Statis.Ran();
+                   
+                }
+                catch(DivideByZeroException ex1)
+                {
+
+                }
+                catch(OutOfMemoryException ex3)
+                {
+
+                }
+                catch(Exception ex2)
+                {
+
+                }
 
 
             Week8_4_Static week8_4_Static = new Week8_4_Static();
@@ -42,7 +77,12 @@ namespace mm_cs_Reference
             Lunapark.IceCream ice;
             Park.IceCream park;
 
+            }
+            catch (Exception ex)
+            {
 
+                Console.WriteLine(ex.ToString());
+            }
         }
     }
 }
---
+++ mm-cs-Reference/Week10_2_Exception.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mm_cs_Reference
+{
+    internal class Week10_2_Exception
+    {
+        private Week10_2_Exception()
+        {
+
+        }
+
+        static readonly Week10_2_Exception _instance  = new Week10_2_Exception();
+
+        public static Week10_2_Exception Instance
+        {
+            get { return _instance; }
+            
+        }
+
+        public int  test(int a,int b)
+        {
+            int c=-1;
+
+            int g = 909;
+            for (int i = 0; i < 20; i++)
+            {
+
+            }
+            try
+            {
+
+                System.IO.File.ReadAllText(@"c:\kuku1.txt");
+                c = a / b;
+            }
+            catch (DivideByZeroException ex1)
+            {
+                c = 0;
+            }
+            catch (FileNotFoundException ex4)
+            {
+
+            }
+            catch (OutOfMemoryException ex3)
+            {
+
+            }
+            catch (Exception ex2)
+            {
+                 
+            }
+
+
+
+            //g = 89;
+            return c;
+        }
+
+    }
+}
