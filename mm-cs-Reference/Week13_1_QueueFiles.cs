bf7c9087336c9058ec728a270c4146d7ef4f13ef
haim hod gilboa <haim@newclass.co.il>
2022-12-18 12:32:46 +0200

    Queue

---
+++ mm-cs-Reference/Week13_1_QueueFiles.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mm_cs_Reference
+{
+    internal class Week13_1_QueueFiles
+    {
+
+        // Main manager
+        // In Main Manager Init
+        // New Entity MyQueue
+        public void Run()
+        {
+            Queue<Item> queue = new Queue<Item>();
+        }
+        public class Item
+        {
+            public string Val { get; set; }
+        }
+
+    }
+}
---
+++ mm-cs-Reference/Week13_1_Threading2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mm_cs_Reference
+{
+    internal class Week13_1_Threading2
+    {
+    }
+}
