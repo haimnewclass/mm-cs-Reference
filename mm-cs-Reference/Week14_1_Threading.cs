a8d8df08933d5cd2e1e45dae9b6fd59939ec4b3a
haimnewclass <haim@newclass.co.il>
2023-01-22 13:54:39 +0200

    Threading

--- mm-cs-Reference/Week14_1_Threading.cs
+++ mm-cs-Reference/Week14_1_Threading.cs
@@ -11,29 +11,31 @@ namespace mm_cs_Reference
     {
         public void Run()
         {
-            Count c1 = new Count("A");
-            Count c2 = new Count("B");            
+            Count c1 = new Count("A",1000);
+            Count c2 = new Count("B",500);            
         }
 
     }
 
     class Count 
     {
-        public Count(string label)
+        public Count(string label,int sleep)
         {
             Label = label;
+            SleepTime = sleep;
             thread = new Thread(Run);
             thread.Start();
         }
 
         public string Label { get; set; }
+        public int SleepTime { get; set; }
 
         public void Run()
         {
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine($" This is num {i} {Label}  ");
-                System.Threading.Thread.Sleep(1000);
+                System.Threading.Thread.Sleep(SleepTime);
             }
         }
 
