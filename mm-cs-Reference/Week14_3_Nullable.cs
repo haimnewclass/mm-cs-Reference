26e76110c5ff6ac147614bb0102a81f31d2c7181
haim hod gilboa <haim@newclass.co.il>
2023-01-25 09:52:36 +0200

    ss

---
+++ mm-cs-Reference/Week14_3_Nullable.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mm_cs_Reference
+{
+    internal class Week14_3_Nullable
+    {
+        public void Run()
+        {
+            // Nullable variables
+            int a=0;
+            int[] arr = new int[10];
+
+            int? b;
+            b = null;
+            b = 12;
+            b = null;
+
+            bool? c = null;
+
+            if(b==null)
+            {
+
+            }
+
+            arr = null;
+            if (arr == null)
+            {
+
+            }
+
+            if (a == 0)
+            {
+
+            }
+
+        }
+    }
+}
