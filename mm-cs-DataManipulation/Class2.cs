99e2f212897c7be57a4f3df8b535588667b2069c
haimnewclass <haim@newclass.co.il>
2023-03-06 14:28:36 +0200

    Model

--- mm-cs-DataManipulation/Class2.cs
+++ mm-cs-DataManipulation/Class2.cs
@@ -8,69 +8,96 @@ namespace mm_cs_DataManipulation
 {
     internal class Class2
 
-        public class Rootobject
-        {
-            public Employee[] employees { get; set; }
-        }
 
-        public class Employee
-        {
-            public string firstName { get; set; }
-            public string lastName { get; set; }
-        }
-
-
-        public class Rootobject
-        {
-            public Employee employee { get; set; }
-        }
-
-        public class Employee
-        {
-            public string name { get; set; }
-            public int salary { get; set; }
-            public bool married { get; set; }
-        }
-
-
-        public class Rootobject
-        {
-            public Glossary glossary { get; set; }
-        }
-
-        public class Glossary
-        {
-            public string title { get; set; }
-            public Glossdiv GlossDiv { get; set; }
-        }
-
-        public class Glossdiv
-        {
-            public string title { get; set; }
-            public Glosslist GlossList { get; set; }
-        }
-
-        public class Glosslist
-        {
-            public Glossentry GlossEntry { get; set; }
-        }
-
-        public class Glossentry
-        {
-            public string ID { get; set; }
-            public string SortAs { get; set; }
-            public string GlossTerm { get; set; }
-            public string Acronym { get; set; }
-            public string Abbrev { get; set; }
-            public Glossdef GlossDef { get; set; }
-            public string GlossSee { get; set; }
-        }
-
-        public class Glossdef
-        {
-            public string para { get; set; }
-            public string[] GlossSeeAlso { get; set; }
-        }
-
+public class Rootobject
+    {
+        public Class1[] Property1 { get; set; }
     }
+
+    public class Class1
+    {
+        public int OrderID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string CustomerID { get; set; }
+        public Customer[] Customers { get; set; }
+    }
+
+    public class Customer
+    {
+        public string ContactName { get; set; }
+        public Shipper[] Shippers { get; set; }
+    }
+
+    public class Shipper
+    {
+        public string CompanyName { get; set; }
+    }
+
+
+
+    public class Rootobject
+    {
+        public Employee[] employees { get; set; }
+    }
+
+    public class Employee
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+
+
+    public class Rootobject
+    {
+        public Employee employee { get; set; }
+    }
+
+    public class Employee
+    {
+        public string name { get; set; }
+        public int salary { get; set; }
+        public bool married { get; set; }
+    }
+
+
+    public class Rootobject
+    {
+        public Glossary glossary { get; set; }
+    }
+
+    public class Glossary
+    {
+        public string title { get; set; }
+        public Glossdiv GlossDiv { get; set; }
+    }
+
+    public class Glossdiv
+    {
+        public string title { get; set; }
+        public Glosslist GlossList { get; set; }
+    }
+
+    public class Glosslist
+    {
+        public Glossentry GlossEntry { get; set; }
+    }
+
+    public class Glossentry
+    {
+        public string ID { get; set; }
+        public string SortAs { get; set; }
+        public string GlossTerm { get; set; }
+        public string Acronym { get; set; }
+        public string Abbrev { get; set; }
+        public Glossdef GlossDef { get; set; }
+        public string GlossSee { get; set; }
+    }
+
+    public class Glossdef
+    {
+        public string para { get; set; }
+        public string[] GlossSeeAlso { get; set; }
+    }
+
+}
 }
